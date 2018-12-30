dfe5de984bc03bd15cb3afa70325d300fca94a2b
dusantodorovic1989 <dusantodorovic1989@gmail.com>
2018-12-30 02:50:09 +0100

    small changes1

--- app/Comment.php
+++ app/Comment.php
@@ -19,13 +19,17 @@ class Comment extends Model
     {
         return $this->belongsTo(Gallery::class);
     }
-    public static function addComment($request , $gallery_id) {
+    public static function addComment($request , $galleryId) {
         $user = auth()->user()->id;
         $comment = new Comment();
         $comment->body = $request->body;
-        $comment->gallery_id = $gallery_id;
+        $comment->gallery_id = $galleryId;
         $comment->user_id = $user;
         $comment->save();
+
+        $newComment = Comment::with('user')->find($comment->id);
+
+        return $newComment;
         
         
        
--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -8,82 +8,19 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function __construct() {
+        $this->middleware('auth:api');
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(CommentRequest $request, $galleryId)
     {
-        //
+        return Comment::addComment($request, $galleryId);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(CommentRequest $request, $gallery_id)
-    {
-        return Comment::addComment($request, $gallery_id);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $comment = Comment::find($id);
-
         $comment->delete();
-        return $comment;
+        return response()->json([
+            'message' => 'Deleted'
+        ]);
     }
 }
